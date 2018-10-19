f1b32c98a8f463cdb8c7819eedf9fd2bcb4c81a0
lixiangyun <linimbus@126.com>
2018-10-19 22:47:46 +0800

    adjust

--- pilot-vm/download-binary.sh
+++ pilot-vm/download-binary.sh
@@ -8,10 +8,10 @@ docker cp $CONTAINER_ID:/usr/local/bin/pilot-discovery ./
 CONTAINER_ID=$(docker ps | grep consul_consul_1 | awk  '{print $1}' )
 docker cp $CONTAINER_ID:/bin/consul ./consul/
 
-CONTAINER_ID=$(docker ps | grep consul_istio-apiserver_1 | awk  '{print $1}' )
+CONTAINER_ID=$(docker ps | grep istio-apiserver_1 | awk  '{print $1}' )
 docker cp $CONTAINER_ID:/usr/local/bin/kube-apiserver ./kube-apiserver/
 
-CONTAINER_ID=$(docker ps | grep consul_productpage-v1-sidecar_1 | awk  '{print $1}' )
+CONTAINER_ID=$(docker ps | grep productpage-v1-sidecar_1 | awk  '{print $1}' )
 docker cp $CONTAINER_ID:/usr/local/bin/pilot-agent ./pilot-agent/
 docker cp $CONTAINER_ID:/usr/local/bin/envoy ./pilot-agent/
 
